8c14cb1f130131770109572079d70a04edd4ac3f
sarit-moses <sarit5761@gmail.com>
2024-11-12 08:22:41 +0200

    modified files

--- day02/rectangle.py
+++ day02/rectangle.py
@@ -1,2 +1,17 @@
-print("Hello World")
+def rec_area(width: float, length: float) -> float:
+    """ this function receives the width and the length of a rectangle, 
+    and returns its area"""
+    return width*length
 
+def rec_circumference(width: float, length: float) -> float:
+    """ this function reveives the width and the length of a rectangle, 
+    and returns its circumference"""
+    return 2*(width+length)
+
+def print_rec_areas(width: float, length: float):
+    """ this function prints the area and circumference of a giver rectangle """
+    print("rectangle area:", rec_area(width, length))
+    print("rectangle circumference:", rec_circumference(width, length))
+
+
+print_rec_areas(3, 4)
