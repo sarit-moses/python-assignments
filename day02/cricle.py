2666e0ad4d7c55cd498f1dd774ede71f7e6fcaa2
sarit-moses <sarit5761@gmail.com>
2024-11-12 08:39:56 +0200

    modified files, I finally understood what was the assignment

--- day02/cricle.py
+++ day02/cricle.py
@@ -1,26 +1,10 @@
 import math
+
+### values:
+radius = 6
 pi_val = math.pi
 
-def cir_area(radius: float) -> float:
-    """ this function receives the width and the length of a circle, 
-    and returns its area"""
-    return pi_val*(radius**2)
-
-def cir_circumference(radius: float) -> float:
-    """ this function reveives the width and the length of a circle, 
-    and returns its circumference"""
-    return 2*pi_val*radius
-
-def print_cir_areas(radius: float):
-    """ this function prints the area and circumference of a giver circle """
-    print("circle area:", cir_area(radius))
-    print("circle circumference:", cir_circumference(radius))
-
-
-
-print(cir_area(1))
-print(cir_circumference(1))
-
-print_cir_areas(1)
-
-
+# print area:
+print("circle area:", pi_val*(radius**2))
+# print circumference:
+print("circle circumference:", 2*pi_val*radius)
